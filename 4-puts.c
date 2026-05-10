83d7b11e1e513270b5484bf448b778c390d9a864
Omegalinksse <omegalinksse@gmail.com>
2026-05-10 02:20:31 +0100

    implement puts for basic out in c

---
+++ 4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Author: ANEHI GODWIN OHINOYI
+ * Program: WinMingle Community C Training
+ * Description: introduction to puts function that output declaration.
+ */
+
+/* entey point */
+int main(void)
+{
+
+/* puts function declaring a output */	
+puts("\"Programming is like building a multilingual puzzle\n");
+
+/* return function to return 0 output */
+	return(0);
+}

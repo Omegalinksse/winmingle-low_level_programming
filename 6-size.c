5e13bbd91bc05e259305d0d8d001d192daec6e89
Omegalinksse <omegalinksse@gmail.com>
2026-05-10 02:30:29 +0100

    Add C Program to print variable sizes

---
+++ 6-size.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * Author: ANEHI GODWIN OHINOYI
+ * Program: WinMingle Community C Training
+ * Description: introduction to size of various data type.
+ */
+
+/* entery point */
+int main(void)
+}
+
+/* printf funtion declaring the size type of various data type*/
+
+/* printf function for character data type size*/
+printf("size of a char: %zu bytes(s)\n",sizeof (char));
+
+/*printf function for integers data type size*/
+printf("size of a int: %zu bytes(s)\n",sizeof (int));
+
+/*printf function for long integers data type sixe */
+printf("size of a long int: %zu bytes(s)\n",sizeof(long int));
+
+/*printf funtion for long long integer data type size*/
+printf("size of a long long int: %zu bytes(s)\n",sizeof(long long int));
+
+/* printf function for float data type size*/
+printf("size of a float: %zu bytes(s)\n",sizeof (float));
+
+/* return function e.g returns 0*/
+return(0);
+}

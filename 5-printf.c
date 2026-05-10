c61b6d352d32c5f280e61279dfa59884ac0c84b1
Omegalinksse <omegalinksse@gmail.com>
2026-05-10 02:22:56 +0100

    Program demonstrating the printf function

---
+++ 5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Author: ANEHI GODWIN OHINOYI
+ * Program: WinMingle Community C Training
+ * Description: introduction to  printf function with line break.
+ */
+
+/* entry point */
+int main(void)
+{
+
+/* printf function declaration for output */
+	printf("with proper grammar, but the outcome is a piece of art,This is how to write a new line\n");
+
+/* return function for returning respond of 0 */
+		return(0);
+}

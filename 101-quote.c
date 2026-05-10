ac145054005089858e30a5f36211139a826b3075
Omegalinksse <omegalinksse@gmail.com>
2026-05-10 02:34:38 +0100

    Program demonstrating the write function

---
+++ 101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * Author: ANEHI GODWIN OHINOYI
+ * Program: WinMingle Community C Training
+ * Description: introduction to write function for declaring output
+ */
+
+/* entry point */
+int main(void)
+{	
+
+/* write function to declare a output */
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+
+/* return function to return error msg */
+return(1);
+}

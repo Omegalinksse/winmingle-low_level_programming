e6a2bdd77bfd6606c478d2b5b86cc7c620ef15e8
Omegalinksse <omegalinksse@gmail.com>
2026-06-04 09:44:28 +0100

    Add 0x00-hello_world folder

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ /**
+ * Author: ANEHI GODWIN OHINOYI
+ * Program: WinMingle Community C Training
+ * Description: This is a printf function that output
+ *the content hello, world with a line break.
+ */
+
+/* entry point */
+int main(void)
+{
+
+/*printf function with the labelled content*/
+printf("hello, world\n");
+
+/* return value function to return 0 */
+return(0);
+}
